89623b09da0b02464e1f1b45e3fc4410bf712f6c
Abhishek Kundar <itsabhikundar17@gmail.com>
2026-07-04 20:57:49 +0530

    Time: 4 ms (46.08%), Space: 12.7 MB (39.38%) - LeetHub

--- 1920-build-array-from-permutation/1920-build-array-from-permutation.py
+++ 1920-build-array-from-permutation/1920-build-array-from-permutation.py
@@ -1,10 +1,8 @@
 class Solution(object):
     def buildArray(self, nums):
         a=[0]*len(nums)
-        k=0
-        for i in nums:
-            a[k]=nums[i] 
-            k+=1   
+        for i in range(len(nums)):
+            a[i]=nums[nums[i]]
         return a
         """
         :type nums: List[int]
